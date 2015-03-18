1edc81321e6f1af44071e5f3d2d59a058b51d1fd
Daniel Brooker <dan@nocturnalcode.com>
2015-03-18 17:00:57 +1300

    remove acts as indexed because this decorator clobbers other calls to acts_as_indexed

--- app/decorators/models/refinery/image_decorator.rb
+++ app/decorators/models/refinery/image_decorator.rb
@@ -3,7 +3,7 @@ require 'acts_as_indexed'
 begin
   Refinery::Image.class_eval do
     # Docs for acts_as_indexed http://github.com/dougal/acts_as_indexed
-    acts_as_indexed :fields => [:title]
+    # acts_as_indexed :fields => [:title]
   end
 rescue NameError
 end
